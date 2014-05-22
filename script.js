d1dc7a3417bc21e0872dea27d7b509d3b612e303
KaiserCreaux <creaux@gmail.com>
2014-05-22 13:58:14 -0400

    0.3.2
    Added disclaimer, comments on my code and listed plans for future
    updates.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-    $tabs = $(".tabs li");
+    $tabs = $(".tabs li");//this manages the three main pages of the site
     
     $tabs.click(function() {
         if($(this)){
-            $tabs.removeClass("selected");
-        $(this).addClass("selected");}
+            $tabs.removeClass("selected");//makes sure nothing can retain 'selected' status after a new click input
+        $(this).addClass("selected");}//re-establishes 'selected' on active li
         
         selectionId = $(this).attr('id');
         
-        $seg = $(".seg#"+selectionId);
-    $(".mContent").fadeOut("fast", function(){
-        $(".seg").css("display", "none");
-        $(".seg#"+selectionId).css("display", "block");
-        $(".mContent").fadeIn("slow");
+        $seg = $(".seg#"+selectionId);//creates object from whichever element of class seg was clicked by %tabs
+    $(".mContent").fadeOut("fast", function(){//removes active main content page from view, clearing the way for the called main content page
+        $(".seg").css("display", "none");//gives all .seg class elements invisibility powers
+        $(".seg#"+selectionId).css("display", "block");//brings the .seg class that is linked to selectionId back into view
+        $(".mContent").fadeIn("slow");//gives instruction on to how to bring it back in
     });
     
     });
-    $tabsPlayers = $(".tabsPlayers li");
+    $tabsPlayers = $(".tabsPlayers li");//this manages the pages for individual players, and is highly similar to the above code.
     
     $tabsPlayers.click(function() {
         if($(this)){
